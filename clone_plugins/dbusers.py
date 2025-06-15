e45b918a1d72fa9495a30a1eded818f4346a03e2
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2025-06-15 14:21:33 +0530

    Update dbusers.py

--- clone_plugins/dbusers.py
+++ clone_plugins/dbusers.py
@@ -1,29 +1,36 @@
 import motor.motor_asyncio
-from config import CDB_NAME, CLONE_DB_URI
+from config import DB_NAME, DB_URI
 
 class Database:
     
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
+        self.col = self.db.users
 
-    async def add_user(self, bot_id, user_id):
-        user = {'user_id': int(user_id)}
-        await self.db[str(bot_id)].insert_one(user)
+    def new_user(self, id, name):
+        return dict(
+            id = id,
+            name = name,
+        )
     
-    async def is_user_exist(self, bot_id, id):
-        user = await self.db[str(bot_id)].find_one({'user_id': int(id)})
+    async def add_user(self, id, name):
+        user = self.new_user(id, name)
+        await self.col.insert_one(user)
+    
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id':int(id)})
         return bool(user)
-    
-    async def total_users_count(self, bot_id):
-        count = await self.db[str(bot_id)].count_documents({})
+
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
         return count
+    
+    async def get_all_users(self):
+        return self.col.find({})
 
-    async def get_all_users(self, bot_id):
-        return self.db[str(bot_id)].find({})
-
-    async def delete_user(self, bot_id, user_id):
-        await self.db[str(bot_id)].delete_many({'user_id': int(user_id)})
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
 
 
-clonedb = Database(CLONE_DB_URI, CDB_NAME)
+db = Database(DB_URI, DB_NAME)
