d32dca284a19e5d929c775eb4298b1b376c87797
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2025-06-16 12:38:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,10 +24,10 @@ BOT_USERNAME = environ.get("BOT_USERNAME", "XYZGetModBot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
-CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
+CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://asdfghjkl:asdfghjkl@cluster0.nof2dzf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
@@ -42,7 +42,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002142649224"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002849749742"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
