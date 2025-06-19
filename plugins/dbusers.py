c87bce38be8dc420e5a8a7ea21d4652e32feddaf
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2025-06-19 10:26:38 +0530

    Update dbusers.py

--- plugins/dbusers.py
+++ plugins/dbusers.py
@@ -2,6 +2,21 @@ import motor.motor_asyncio
 from config import DB_NAME, DB_URI
 
 class Database:
+    async def has_premium_access(self, user_id):
+        user_data = await self.get_user(user_id)
+        if user_data:
+            expiry_time = user_data.get("expiry_time")
+            if expiry_time is None:
+                # User previously used the free trial, but it has ended.
+                return False
+            elif isinstance(expiry_time, datetime.datetime) and datetime.datetime.now() <= expiry_time:
+                return True
+            else:
+                await self.users.update_one({"id": user_id}, {"$set": {"expiry_time": None}})
+        return False
+
+    async def update_user(self, user_data):
+        await self.users.update_one({"id": user_data["id"]}, {"$set": user_data}, upsert=True)
     
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
@@ -31,6 +46,9 @@ class Database:
 
     async def delete_user(self, user_id):
         await self.col.delete_many({'id': int(user_id)})
+
+
+
 
 
 db = Database(DB_URI, DB_NAME)
