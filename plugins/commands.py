e0dd3710158cc1f91625b3be4c1ab34ea7a353b2
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2025-06-22 12:02:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -216,7 +216,7 @@ async def start(client, message):
 
 
     pre, decode_file_id = ((base64.urlsafe_b64decode(data + "=" * (-len(data) % 4))).decode("ascii")).split("_", 1)
-    if not await check_verification(client, message.from_user.id) and VERIFY_MODE == True:
+    if await check_verification(client, message.from_user.id) and VERIFY_MODE == True:
         btn = [[
             InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/{username}?start="))
         ],[
